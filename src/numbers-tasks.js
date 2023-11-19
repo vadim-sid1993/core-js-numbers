ae6aeac410924f6011d519e8e870aea41b6d6f07
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2023-11-19 20:20:07 +0300

    Add files via upload

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -262,8 +262,10 @@ function toNumber(value, def) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  // throw new Error('Not implemented');
+  const result = num ** 3;
+  return result;
 }
 
 /**
